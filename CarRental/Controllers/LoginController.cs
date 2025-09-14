e9453d17186ea4e352253075c2e0e74c96272bef
Kalaimakan06 <kalaimakan1510@gmail.com>
2025-09-14 16:23:31 +0530

    Login start

---
+++ CarRental/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+
+    }
+}
---
+++ CarRental/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CarRental.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Identifier { get; set; }
+        public string Password { get; set; }
+    }
+}
