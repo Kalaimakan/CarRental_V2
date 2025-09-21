fdc35c158ff2ad9b2958ef287a93e4cacf094a1a
Kalaimakan06 <kalaimakan1510@gmail.com>
2025-09-21 12:39:36 +0530

    customer dashboard design polish

--- CarRental/Models/Contact.cs
+++ CarRental/Models/Contact.cs
@@ -7,16 +7,20 @@ namespace CarRental.Models
         [Key]
         public Guid Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set; }
 
-        [Required, EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Subject is required")]
+        [StringLength(100, ErrorMessage = "Subject cannot exceed 100 characters")]
         public string Subject { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Message is required")]
+        [StringLength(500, ErrorMessage = "Message cannot exceed 500 characters")]
         public string Message { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
--- CarRental/ViewModels/ContactViewModel.cs
+++ CarRental/ViewModels/ContactViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using CarRental.DTOs;
+using CarRental.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarRental.ViewModels
 {
     public class ContactViewModel
     {
-        public ContactDto Contact { get; set; }
+        public ContactDto Contact { get; set; } = new ContactDto(); 
         public string SuccessMessage { get; set; }
     }
 }
